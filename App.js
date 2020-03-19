8ecaa205e594ffc8519a21ca564d3e84f9311ddb
kbh0581 <kbh0581@gmail.com>
2020-03-19 22:59:16 +0900

    Add loading View and Container View

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Alert} from "react-native";
+import Loading from "./loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on ss your app!</Text>
-    </View>
-  );
+
+export default class extends React.Component {
+  state ={
+    isLoding : true
+  }
+  getLocation = async()=>{
+    try{
+    
+    const response = await Location.requestPermissionsAsync();  //권한요청 안한다면 에러발생
+    const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync();
+    this.setState({isLoding:false})
+    //Todo :: wether API Use
+    
+    }catch(err){
+      Alert.alert("Can't find you","So Sad"); //권한을 안주면 울음
+
+    }
+  }
+
+  componentDidMount(){
+    this.getLocation();
+    
+  }
+  render(){
+    const {isLoding} = this.state;
+    return (isLoding ? <Loading/> : null);
+  };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
